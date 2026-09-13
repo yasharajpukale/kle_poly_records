921ae209c12e0c5f5f575c7e1808557154243dac
yasharajpukale <yashrajprojects09@gmail.com>
2026-09-13 23:27:06 +0530

    app launcher reupdated

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "78589240" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3476719550" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
