678f8dc07761addc1662f0a3bcf0552cc1c03a3b
yasharajpukale <yashrajprojects09@gmail.com>
2026-09-13 23:54:56 +0530

    web rebuilt second time

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -35,8 +35,9 @@ if (!window._flutter) {
 }
 _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27da0","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
 
+
 _flutter.loader.load({
-  serviceWorkerSettings: {
-    serviceWorkerVersion: "1217607003" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
-  }
+  config: {
+    canvasKitBaseUrl: "canvaskit/",
+  },
 });
